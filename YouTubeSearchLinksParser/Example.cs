29417d677e8f88131b6b805155ad8f2ff4a9e92d
Midnight_Insomnia <nekit.f.a@mail.ru>
2020-06-22 03:53:57 +0300

    D0N3

---
+++ YouTubeSearchLinksParser/Example.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace YouTubeSearchLinksParser
+{
+    class Example
+    {
+        static void Main(string[] args)
+        {
+            //Создаём объект класса и передаём конструктору поисковый запрос
+            YTParser parser = new YTParser(videoName: "you're gonna go far kid");
+            //Пользуемся результатом
+            var result = parser.GetLinksAsync().Result;
+            Console.WriteLine(result[0]);
+            Console.WriteLine(result.Count);
+        }
+    }
+}
---
+++ YouTubeSearchLinksParser/YTParser.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace YouTubeSearchLinksParser
+{
+    public class YTParser
+    {
+        private static List<string> videoLinks = new List<string>();
+        private static string query;
+        public static string Query
+        {
+            get
+            {
+                return query;
+            }
+            set
+            {
+                //Меняет символы в запросе на + для корректного построения ссылки
+                System.Text.StringBuilder sb = new System.Text.StringBuilder(value);
+
+                for (int i = 0; i < sb.Length; i++)
+                {
+                    if (Char.IsSymbol(sb[i]) || Char.IsPunctuation(sb[i]) || Char.IsSeparator(sb[i]))
+                    {
+                        sb[i] = '+';
+                    }
+                }
+                query = sb.ToString();
+            }
+        }
+        public YTParser(string videoName)
+        {
+            Query = videoName;
+        }
+        public async Task<List<string>> GetLinksAsync()
+        {
+            string url = $"https://www.youtube.com/results?search_query={query}";
+            ////Получение HTML документа
+            var httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            foreach (HtmlNode link in htmlDocument.DocumentNode.SelectNodes("//a[@href]"))
+            {
+                HtmlAttribute att = link.Attributes["href"];
+                //Отбирает только ссылки на видео                                
+                if (att.Value.Contains("/watch?v="))
+                {
+                    videoLinks.Add(att.Value);
+                }
+            }
+            return videoLinks;
+        }
+    }
+}
